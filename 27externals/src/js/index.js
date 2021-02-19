52e93e49c45296d7e9e5b8ff345742a98cb37a04
yinxiaolong-1123 <1642990045@qq.com>
2021-02-19 18:24:04 +0800

    map

--- 27externals/src/js/index.js
+++ 27externals/src/js/index.js
@@ -1,3 +1,35 @@
 import $ from 'jquery';
 
 console.log($);
+
+
+function yin() {
+    let arr = [
+        {
+            name: 'joke',
+            id: 1
+        },
+        {
+            name: 'joke',
+            id: 2
+        },
+        {
+            name: 'joke',
+            id: 3
+        },
+        {
+            name: 'joke',
+            id: 4
+        },
+        {
+            name: 'joke',
+            id: 5
+        },  
+    ]
+    arr = arr.map(itme => {
+        return itme.id
+    })
+    console.log(arr);
+}
+yin();
+
